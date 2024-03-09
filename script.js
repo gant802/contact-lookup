b14cef370c24723ae57aa331da8163cb23005bde
Grant Cummings <grantcummings4429@gmail.com>
2024-03-09 13:20:42 -0500

    local storage linked

--- script.js
+++ script.js
@@ -11,63 +11,38 @@ const stateText = document.getElementById('state');
 const zipCodeText = document.getElementById('zip-code');
 const submitButton = document.getElementById('submit-button');
 
-const contacts = [
-    {
-        firstName: "Quinn",
-        lastName: "O'reilly",
-        mobileNumber: 8028640201,
-        email: "oreillyqb@gmail.com",
-        address: {
-            street: "39 Maple Street",
-            appartmentNumber: "B",
-            city: "Winooski",
-            state: "Vermont",
-            zipCode: "05404"
-        }
-    },
-    {
-        firstName: "Brennan",
-        lastName: "Fitzpatrick",
-        mobileNumber: 8024469087,
-        email: "Fitzy7@gmail.com",
-        address: {
-            street: "44 Allen Drive",
-            appartmentNumber: "",
-            city: "South Burlington",
-            state: "Vermont",
-            zipCode: "05403"
+const contacts = JSON.parse(localStorage.getItem('contacts')) || [];
+
+function searchForContact(input, array) {
+    const matchingContacts = [];
+    for (let contact of array) {
+        for (let key in contact) {
+            if (typeof contact[key] === 'object') {
+                // Recursively search nested objects
+                const result = searchForContact(input, [contact[key]]); // Convert nested object to array for recursion
+                if (result && result.length > 0) {
+                    matchingContacts.push(contact);
+                }
+            } else {
+                const formattedValue = contact[key].toString().toLowerCase();
+                if (formattedValue.includes(input)) {
+                    matchingContacts.push(contact);
+                    break; // Stop searching this contact once a match is found
+                }
+            }
         }
     }
-];
-
-
-function searchForContact(input, obj) {
-    for (let key in obj) {
-        if (typeof obj[key] === 'object') {
-            const result = searchForContact(input, obj[key]); // Recursively search nested objects
-            if (result) {
-                return obj;
-            }
-        } else {
-            const formattedValue = obj[key].toString().toLowerCase();
-            if (formattedValue.includes(input)) {
-                return obj;
-            }
-        } 
-    }
-    return null;
-}
+    return matchingContacts.length > 0 ? matchingContacts : false; // Return array of matching contacts
+};
 
 searchButton.addEventListener("click", function() {
-    const input = searchInput.value.toLowerCase();
-    for (let i = 0; i < contacts.length; i++) {
-    let result = searchForContact(input, contacts[i]); // Start searching from the first contact
+    const userInput = searchInput.value.toLowerCase();
+    let result = searchForContact(userInput, contacts); // Start searching from the first contact
     if (result) {
         console.log(result);
-    } else if (i === contacts.length - 1){
+    } else {
         return console.log("Cannot find contact")
     }
-} 
 });
 
 function addContact() {
@@ -86,18 +61,32 @@ function addContact() {
     };
     if (firstNameText.value !== '' && lastNameText.value !== '' && mobileNumberText.value !== '') {
         contacts.push(contactObj);
+        localStorage.setItem('contacts', JSON.stringify(contacts));
         console.log(contacts);
     } else {
         console.log("Needs at least a first name, last name and mobile number!")
     }
 };
 
+function clearContent() {
+    firstNameText.value = "";
+    lastNameText.value = "";
+    mobileNumberText.value = "";
+    emailText.value = "";
+    streetText.value = "";
+    appartmentNumberText.value = "";
+    cityText.value = "";
+    stateText.value = "";
+    zipCodeText.value = "";
+}
+
 
 submitButton.addEventListener("click", (event) => {
     event.preventDefault();
     addContact();
+    clearContent();
 });
 
 console.log(contacts);
 
-//When you come back to this, add it to github and use local storage to save data.
+
